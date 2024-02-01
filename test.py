cbf6996c137f498e6a75742541b68c87757836fd
Michel Kunkler <michel.kunkler@tum.de>
2024-02-01 09:33:51 +0100

    pickle model

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import pickle
 from simulator.simulator import Simulator
 from planner import Planner
 from policy import *
@@ -5,7 +6,7 @@ from task_execution_time import ExecutionTimeModel
 
 prediction_model = ExecutionTimeModel()
 warm_up_policy = RandomPolicy()
-warm_up_time =  24
+warm_up_time =  24*365
 #policy = RandomPolicy()
 policy = HungarianPolicy()
 policy = GreedyParallelMachinesSchedulingPolicy()
@@ -13,5 +14,9 @@ my_planner = Planner(prediction_model, warm_up_policy, warm_up_time, policy,
                      predict_multiple=False)
 
 simulator = Simulator(my_planner)
-result = simulator.run(3*24)
+result = simulator.run(warm_up_time)
+
+with open('prediction_model.pkl', 'wb') as file:
+    pickle.dump(prediction_model, file)
+
 print(result)
