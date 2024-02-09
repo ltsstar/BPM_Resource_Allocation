f2bb9a524df552d8b2ecaf66e70f1d2062ea2df5
Michel Kunkler <michel.kunkler@tum.de>
2024-02-09 17:25:34 +0100

    python3.10 in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo dnf install python3-pip gcc pipenv -y
-curl https://pyenv.run | bash
+sudo dnf install python3.10 python3-pip gcc pipenv -y
+su -curl https://pyenv.run | bash
 export PIPENV_YES=1
 pipenv install
