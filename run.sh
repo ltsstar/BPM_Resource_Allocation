9b150b730bf25e5c3cd2807eaa0220ca1b94130b
ltsstar <michel.kunkler@gmail.com>
2024-06-18 08:46:57 +0200

    better run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #                        start   end   stepsize   Algorithm   days   processes    selection stragegy problem
-python -u src/test.py      1.1   1.01      0.001        Hungarian      365        4             slowest PO       2>&1 | tee out.txt
+python -u src/test.py      1.0   1.1      0.01        Hungarian      365        4             slowest PO       2>&1 | tee out.txt
